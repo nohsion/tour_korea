aee794774aad8b1535fa6760ee80b54ba659f086
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-04 23:40:58 +0900

    Rendering the contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from "axios";
+import Content from "./Content";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        contents: []
+    }
+
+    getLocation = async () => {
+        const {data: {response: {body: {items: {item}}}}} = await axios.get(
+            `http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=${process.env.REACT_APP_API_KEY}&numOfRows=17&MobileOS=ETC&MobileApp=AppTest&_type=json`)
+
+        let locations = []
+        for (let i = 0; i < item.length; i++) {
+            locations.push(item[i].name)
+        }
+        console.log(locations)
+
+        // locations[0]: 서울
+        const {data: {response: {body: { items }}}} = await axios.get(
+            `http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?ServiceKey=${process.env.REACT_APP_API_KEY}&keyword=${locations[0]}&arrange=P&MobileOS=ETC&MobileApp=AppTest&_type=json`)
+        console.log(items.item)
+        this.setState( {contents: items.item})
+    }
+
+    componentDidMount() {
+        this.getLocation()
+    }
+
+
+    render() {
+        const { contents } = this.state
+        return (
+            <>
+                <div>{contents.map(content => {
+                    return <Content
+                        tel={content.tel}
+                        firstimage={content.firstimage}
+                        mapx={content.mapx}
+                        contentid={content.contentid}
+                        contenttypeid={content.contenttypeid}
+                        title={content.title}
+                        addr1={content.addr1}
+                        mapy={content.mapy}
+                    />
+                })}</div>
+            </>
+        )
+    }
 }
 
 export default App;
---
+++ src/Content.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+function Content({ title, addr1, addr2, contentid, contenttypeid, firstimage, tel, mapx, mapy}) {
+    return (
+        <div>{title}</div>
+    )
+}
+
+Content.propTypes = {
+    title: PropTypes.string.isRequired,
+    addr1: PropTypes.string.isRequired,
+    addr2: PropTypes.string,
+    contentid: PropTypes.number.isRequired,
+    contenttypeid: PropTypes.number.isRequired,
+    firstimage: PropTypes.string.isRequired,
+    tel: PropTypes.string.isRequired,
+    mapx: PropTypes.number.isRequired,
+    mapy: PropTypes.number.isRequired
+}
+
+export default Content
