b05af2054d775e96b16a860a3e57435f2f86f307
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-05 18:31:21 +0900

    img, title, addr 받아오기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import Content from "./Content";
+import "./App.css"
 
 class App extends React.Component {
     state = {
@@ -8,18 +9,24 @@ class App extends React.Component {
     }
 
     getLocation = async () => {
-        const {data: {response: {body: {items: {item}}}}} = await axios.get(
-            `http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=${process.env.REACT_APP_API_KEY}&numOfRows=17&MobileOS=ETC&MobileApp=AppTest&_type=json`)
+        let url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode' /*URL*/
+        let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + process.env.REACT_APP_API_KEY /*Service Key*/
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('17') /**/
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1') /**/
+        queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC') /**/
+        queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest') /**/
+        queryParams += '&_type=json'
 
+        const {data: {response: {body: {items: {item}}}}} = await axios.get(url + queryParams)
         let locations = []
         for (let i = 0; i < item.length; i++) {
             locations.push(item[i].name)
         }
         console.log(locations)
 
-        // locations[0]: 서울
+        // locations[0]: 서울, numOfRows: 15개,
         const {data: {response: {body: {items}}}} = await axios.get(
-            `http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?ServiceKey=${process.env.REACT_APP_API_KEY}&keyword=${locations[0]}&arrange=P&MobileOS=ETC&MobileApp=AppTest&_type=json`)
+            `http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?ServiceKey=${process.env.REACT_APP_API_KEY}&numOfRows=15&keyword=${locations[0]}&arrange=P&MobileOS=ETC&MobileApp=AppTest&_type=json`)
         console.log(items.item)
         this.setState({contents: items.item})
     }
@@ -32,21 +39,24 @@ class App extends React.Component {
     render() {
         const {contents} = this.state
         return (
-            <>
-                <div>{contents.map(content => (
-                    <Content
-                        key={content.contentid}
-                        tel={content.tel}
-                        firstimage={content.firstimage}
-                        mapx={content.mapx}
-                        contentid={content.contentid}
-                        contenttypeid={content.contenttypeid}
-                        title={content.title}
-                        addr1={content.addr1}
-                        mapy={content.mapy}
-                    />
-                ))}</div>
-            </>
+            <section className="container">
+                <div>
+                    {contents.map(content => (
+                        <Content
+                            key={content.contentid}
+                            tel={content.tel}
+                            firstimage={content.firstimage}
+                            firstimage2={content.firstimage2}
+                            mapx={content.mapx}
+                            contentid={content.contentid}
+                            contenttypeid={content.contenttypeid}
+                            title={content.title}
+                            addr1={content.addr1}
+                            mapy={content.mapy}
+                        />
+                    ))}
+                </div>
+            </section>
         )
     }
 }
