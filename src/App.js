1b1e3808ae35249521a9e1f04cf500ebe84f98e0
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-06 17:07:04 +0900

    반응형 웹, 라우터 연결

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {HashRouter, Route} from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail"
 
+
 function App() {
     return (
         <HashRouter>
             <Route path="/" exact={true} component={Home} />
-            <Route path="/travel" component={Detail} />
+            {/* TODO: path 설정하기 - contenttypeid에 맞는 이름을 가져오고 싶음. Content.js 딕셔너리 참고 */}
+            <Route path="/:contentType[contenttypeid.toString()]/:contentid" component={Detail} />
         </HashRouter>
     )
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,26 +2,35 @@ import React from "react";
 import axios from "axios";
 
 class Detail extends React.Component {
+    state = {
+        infos: []
+    }
 
+    getInfos = async () => {
+        const {location: {state: {contentid}}} = this.props
+        // if (contentid === undefined) {
+        //
+        // }
+        console.log(contentid)
+        /* 공통 정보 조회 */
+        let url_areaCode = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon' /*URL*/
+        let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + process.env.REACT_APP_API_KEY /* Service Key */
+        queryParams += '&' + encodeURIComponent('contentId') + '=' + encodeURIComponent(contentid)
+        queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC')
+        queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest')
+        queryParams += '&_type=json'
 
-    // getLocation = async () => {
-    //     /* 지역 코드 조회 */
-    //     let url_areaCode = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode' /*URL*/
-    //     let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + process.env.REACT_APP_API_KEY /* Service Key */
-    //     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('17')
-    //     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1')
-    //     queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC')
-    //     queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest')
-    //     queryParams += '&_type=json'
-    //
-    //     const {data: {response: {body: {items: {item}}}}} = await axios.get(url_areaCode + queryParams)
-    //     this.setState({locations: item})
-    // }
+        const {data: {response: {body: {items: {item}}}}} = await axios.get(url_areaCode + queryParams)
+        this.setState({infos: item})
+    }
+
     componentDidMount() {
         const {location, history} = this.props
         if (location.state === undefined) {
             history.push("/")
         }
+
+        this.getInfos()
     }
 
     render() {
